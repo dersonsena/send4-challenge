fc9dec6ac807eb4064b147f09cf1327257c5048e
Kilderson Sena <dersonsena@gmail.com>
2020-03-14 18:56:19 -0300

    Criação das migrations de produtcs_users e seu respectivo model

---
+++ app/Domain/Product/ProductUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Product;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductUser extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'products_users';
+}
