3a8adae8ccb4fa8bafe372d21c7fd5ca294a557c
Kilderson Sena <dersonsena@gmail.com>
2020-03-14 20:02:45 -0300

    Criando um construtor opcional para popular um produto do shopify

--- app/Domain/Shopify/Product.php
+++ app/Domain/Shopify/Product.php
@@ -20,6 +20,11 @@ class Product
      */
     private $attributes = [];
 
+    public function __construct(array $attributes = [])
+    {
+        $this->attributes = $attributes;
+    }
+
     public function __get($name)
     {
         if (!isset($this->attributes[$name])) {
