de0549213ae3882f397e9bac481d6e02c3b4c731
Kilderson Sena <dersonsena@gmail.com>
2020-03-14 20:04:05 -0300

    Implementação do serviço de listagem dos favoritos do usuario logado

--- app/Domain/Product/ProductUser.php
+++ app/Domain/Product/ProductUser.php
@@ -2,6 +2,9 @@
 
 namespace App\Domain\Product;
 
+use App\Domain\Shopify\Product;
+use App\Infra\Rest\EndpointResolver;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -18,4 +21,28 @@ class ProductUser extends Model
      * @var string
      */
     protected $table = 'products_users';
+
+    /**
+     * @param int $userId
+     * @return Product[]
+     */
+    public static function getFavoritesList(int $userId): array
+    {
+        /** @var Collection $productsOfTheUser */
+        $productsOfTheUser = static::where(['user_id' => $userId])->get();
+
+        $ids = array_map(function ($row) {
+            return $row->product_id;
+        }, $productsOfTheUser->all());
+
+        $endpoint = EndpointResolver::getApiEndpoint("/products.json?ids=" . implode(',', $ids));
+        $data = (json_decode(file_get_contents($endpoint)))->products;
+        $responseData = [];
+
+        foreach ($data as $row) {
+            $responseData[] = (new Product((array)$row))->getAttributes();
+        }
+
+        return $responseData;
+    }
 }
