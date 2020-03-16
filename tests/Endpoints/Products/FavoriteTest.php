321799f848b14509c9d6cfa054ca0904c316d98d
Kilderson Sena <dersonsena@gmail.com>
2020-03-16 13:46:13 -0300

    Implementação do teste de integração para o Favorite Product

---
+++ tests/Endpoints/Products/FavoriteTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+class FavoriteTest extends AuthTestCase
+{
+    /**
+     * @var string
+     */
+    const URI = '/api/products/favorite/4543367512203';
+
+    /**
+     * @var string
+     */
+    const METHOD = 'POST';
+
+    public function testFavoriteAction()
+    {
+        $this->actingAs($this->user)
+            ->json(static::METHOD, static::URI)
+            ->seeStatusCode(200)
+            ->seeJson([
+                'status' => 'success',
+                'data' => 'Product was favorited successfully.'
+            ]);
+    }
+
+    public function testIfProductIsAlreadyFavorite()
+    {
+        DB::table('products_users')->insert([
+            'user_id' => $this->user->id,
+            'product_id' => 4543367512203
+        ]);
+
+        $this->actingAs($this->user)
+            ->json(static::METHOD, static::URI)
+            ->seeStatusCode(200)
+            ->seeJson([
+                'status' => 'success',
+                'data' => 'This product is already in your favorites list.'
+            ]);
+    }
+}
