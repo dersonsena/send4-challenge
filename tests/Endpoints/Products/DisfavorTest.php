5869290cb45b852c4e3b72273c3c752d97b53fea
Kilderson Sena <dersonsena@gmail.com>
2020-03-16 13:46:23 -0300

    Implementação do teste de integração para o Disfavor Product

---
+++ tests/Endpoints/Products/DisfavorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+class DisfavorTest extends AuthTestCase
+{
+    /**
+     * @var string
+     */
+    const URI = '/api/products/disfavor/4543373377675';
+
+    /**
+     * @var string
+     */
+    const METHOD = 'POST';
+
+    public function testDisfavoriteAction()
+    {
+        DB::table('products_users')->insert([
+            'user_id' => $this->user->id,
+            'product_id' => 4543373377675
+        ]);
+
+        $this->actingAs($this->user)
+            ->json(static::METHOD, static::URI)
+            ->seeStatusCode(200)
+            ->seeJson([
+                'status' => 'success',
+                'data' => 'Product was disfavored successfully.'
+            ]);
+    }
+
+    public function testIfProductIsAlreadyDisfavorite()
+    {
+        $this->actingAs($this->user)
+            ->json(static::METHOD, static::URI)
+            ->seeStatusCode(200)
+            ->seeJson([
+                'status' => 'success',
+                'data' => "This product doesn't in your favorites list."
+            ]);
+    }
+}
