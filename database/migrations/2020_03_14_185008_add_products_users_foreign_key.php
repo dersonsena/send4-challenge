63c5ce3593c4bcb0bb537ad6425a0dd763c0a641
Kilderson Sena <dersonsena@gmail.com>
2020-03-16 14:42:48 -0300

    Corrigind migration para ela não executar drop fk quando for em um sqlite

--- database/migrations/2020_03_14_185008_add_products_users_foreign_key.php
+++ database/migrations/2020_03_14_185008_add_products_users_foreign_key.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddProductsUsersForeignKey extends Migration
@@ -29,7 +30,11 @@ class AddProductsUsersForeignKey extends Migration
     public function down()
     {
         Schema::table('products_users', function (Blueprint $table) {
-            $table->dropForeign('products_users_user_id_foreign');
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign('products_users_user_id_foreign');
+            }
+
+            $table->dropIfExists();
         });
     }
 }
